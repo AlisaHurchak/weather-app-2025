271923bf0318dc44181ceca6f72b8560cd557d91
Alisa Hurchak <alisahurchak@gmail.com>
2025-10-29 15:58:09 +0100

    Added JS date and input value

---
+++ src/script.js
@@ -0,0 +1,32 @@
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#weather-app-city");
+  let input = document.querySelector("#enter-city-input");
+  city.innerHTML = input.value;
+}
+
+let newCity = document.querySelector("#search-from");
+newCity.addEventListener("submit", search);
+
+function formattedDate(date) {
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let formattedDay = days[day];
+  return `${formattedDay} ${hour}:${minutes}`;
+}
+
+let newTime = document.querySelector("#time-now");
+let now = new Date();
+
+newTime.innerHTML = formattedDate(now);
